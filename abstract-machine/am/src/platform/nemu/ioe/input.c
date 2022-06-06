4dd70a753898c549ce53b75cdd17142affe45473
rda <2055017970@qq.com>
2022-06-05 23:28:08 -0700

    keybord

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,9 +1,11 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t data = (uint32_t)inl(KBD_ADDR);
+  kbd->keydown = (data & KEYDOWN_MASK) >> 15;
+  kbd->keycode = data & (~KEYDOWN_MASK);
 }
