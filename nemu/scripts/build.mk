781006939778131118f57309dcfe5e7408091f98
rda <2055017970@qq.com>
2023-01-16 11:18:50 +0800

    edit build.mk

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -32,6 +32,10 @@ $(OBJ_DIR)/%.o: %.c
 	@echo + CC $<
 	@mkdir -p $(dir $@)
 	@$(CC) $(CFLAGS) -c -o $@ $<
+	@$(CC) $(CFLAGS) -E -MF /dev/null $< | \
+		grep -ve '^#' | \
+		clang-format - > $(basename $@).i
+
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 $(OBJ_DIR)/%.o: %.cc
